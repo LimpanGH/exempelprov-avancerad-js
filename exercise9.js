fa8bd15469b879c711cfe704a876fde4dc513488
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-02-07 16:06:17 +0100

    added exercises

--- exercise9.js
+++ exercise9.js
@@ -2,5 +2,10 @@
 // Du ska skapa två klasser med ES6 syntax, Book och Author.
 // Book ska ha följande egenskaper:
 // title, author, rating
+// och metoden 
+// displayBook som visar information om boken
 // Author ska ha följande egenskaper:
-// name, books
+// name, books (en array med böcker som är tom från början)
+// och metoden
+// addBook som lägger till en bok till författarens lista
+// listBooks som visar en lista på författarens böcker
