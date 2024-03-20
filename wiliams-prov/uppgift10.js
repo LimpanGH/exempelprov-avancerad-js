5b805be141eee75637a077533c2c0ee50adf5ae0
LimpanGH <Linusj555@gmail.com>
2024-03-20 15:49:36 +0100

    changes to uppg10

--- wiliams-prov/uppgift10.js
+++ wiliams-prov/uppgift10.js
@@ -1,1 +1,7 @@
+// 10 (3p)
+// Använd ES6 moduler. Du ska exportera klasserna Student och Course genom att skapa filerna Student.js och Course.js.
+// Skapa en tredje fil, main.js, där du importerar klasserna och skapar två olika studenter och lägger till två olika kurser till studenterna.
+// Skriv även ut studententens kurser (listCourses()) samt information om kurserna (getCourseInfo()).
+// main.js ska vara kopplat till index.html och informationen om studenterna och kurserna ska loggas ut i consolen.
+
 // Kolla main.js och student.js
